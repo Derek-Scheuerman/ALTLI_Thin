f7f0a90a422dc51bac1fed3255864ca3afa29fe2
Derek-Scheuerman <87347237+Derek-Scheuerman@users.noreply.github.com>
2026-02-12 22:49:52 -0600

    added inv and removed old code (#7)
    Added new folders for the inv
    Fixed porting issues with the inv
    Updated UI for the inv
    Created basic save and loading

--- Source/ALTLI_Thin/ALTLI_Thin.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ALTLI_Thin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ALTLI_Thin, "ALTLI_Thin" );
-
-DEFINE_LOG_CATEGORY(LogALTLI_Thin)
